cf5d843dd1d5abd51045f81bb8509ac509f96282
Alan Parker Lue <alan.parker.lue@gmail.com>
2017-01-23 20:27:38 -0500

    Add stub Xact class

---
+++ README.md
@@ -0,0 +1,4 @@
+# To do #
+
+- 2017-01-23 Add way to check which transactions do not have Shadow
+  tags
--- ledger-shadow.py
+++ ledger-shadow.py
@@ -4,6 +4,27 @@ import argparse
 import ast
 import re
 
+class Xact:
+    def __init__(self):
+        self.postings = []
+        self.shadow = None
+
+    def clear(self):
+        self.postings = []
+        self.shadow = None
+
+    def add_posting(self, posting):
+        self.postings.append(posting)
+
+    def add_shadow(self, shadow):
+        self.shadow = shadow
+
+    def print(self):
+        if self.shadow:
+            print('    ; {}: {}'.format(type(shadow), shadow))
+        for p in self.postings:
+            print('    [{}]'.format(p))
+
 # Transaction begin pattern
 trbegpat = re.compile(r'^\d')
 # Transaction end pattern
@@ -18,38 +39,34 @@ parser.add_argument('journal', help='Ledger journal file')
 args = parser.parse_args()
 filepath = args.journal
 
+xact = Xact()
 bInXact = False
 with open(filepath) as f:
     for line in f:
         if bInXact:
             if trendpat.match(line):
-                if shadow:
-                    print('    ; {}: {}'.format(type(shadow), shadow))
-                for p in postings:
-                    print('    [{}]'.format(p))
+                xact.print()
                 bInXact = False
             else:
                 m = rpostpat.match(line)
                 if m:
-                    postings.append(m.group(1))
+                    xact.add_posting(m.group(1))
                 else:
                     m = shkeypat.search(line)
                     if m:
                         shadow = ast.literal_eval(m.group(1).strip())
+                        xact.add_shadow(shadow)
 
         if not bInXact and trbegpat.match(line):
             bInXact = True
-            postings = []
-            shadow = None
+            xact.clear()
             m = shkeypat.search(line)
             if m:
                 shadow = ast.literal_eval(m.group(1).strip())
+                xact.add_shadow(shadow)
 
         print(line, end='')
 
 if bInXact:
-    if shadow:
-        print('    ; {}: {}'.format(type(shadow), shadow))
-    for p in postings:
-        print('    [{}]'.format(p))
+    xact.print()
     bInXact = False
