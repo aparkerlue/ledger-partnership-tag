b22c6db398e3382b618e0f01b8c0137ae9fb183f
Alan Parker Lue <alan.parker.lue@gmail.com>
2017-02-12 13:05:48 -0500

    Process shadow tags
    Squashed commit of the following:
    
    commit 857963e34bfd1c6eca5543cc8cd1ea1a6b60c611
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Sun Feb 12 13:04:44 2017 -0500
    
        Correctly prints shadow postings
    
    commit 4bb158288679c46181305166d29eac4e40ee695c
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Sun Feb 12 12:05:52 2017 -0500
    
        Posting classes functional
    
    commit b7c619c4fac1825dfd1aaaa2609e2dcd0b8543af
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Sun Feb 12 10:41:33 2017 -0500
    
        Progress on posting classes
    
    commit 86cfa658827a59fa245b8fe15f34cadd92a2a1ad
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Sat Feb 11 08:10:33 2017 -0500
    
        Inheritance for Posting/RealPosting
    
    commit f495b7ac5ad8b441a6aa56e509377c5dfdafae6f
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Sat Feb 11 08:09:22 2017 -0500
    
        Renamed `ledger-shadow.py` to `ledger_shadow.py`
    
    commit 1d76ae8552449866f6848f86c675b0073daf5453
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Fri Feb 10 20:44:57 2017 -0500
    
        Posting class stubs
    
    commit e71e4f38af48ad1f89419da62e3cb757ac275c40
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Thu Feb 9 11:10:30 2017 -0500
    
        Stub edits to calculate values for last owner
    
    commit ed5606779255d7cbb90037c498b2616baaddb9db
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Thu Feb 9 09:16:38 2017 -0500
    
        Create shadow postings; need to check posting sum
    
    commit 16080f6c71d808736e200c8ca747c5aa945e8fdf
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Wed Feb 8 20:12:01 2017 -0500
    
        Create shadow postings ex owner labels
    
    commit ff66bb98065d8cf2c778c9743e4cb74926135a49
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Wed Feb 8 07:44:12 2017 -0500
    
        Resolve elided postings WIP
    
    commit 1c80c12e59a8e087faa4fe0d8ac02eb6b73db25c
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Tue Feb 7 08:21:31 2017 -0500
    
        Split account and value in posting data structure
    
    commit 0739cd916ed29c0fc7a466b691dab2003875a7fb
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Mon Feb 6 07:45:25 2017 -0500
    
        Function to resolve elided values
    
    commit a591a64e772d99a13f74f46d7902faec383b95f2
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Wed Jan 25 08:45:22 2017 -0500
    
        Writing function to generate shadow postings
    
    commit cdd00bcec53d1cde846eb144eb39d1a82aabf8a5
    Author: Alan Parker Lue <alan.parker.lue@gmail.com>
    Date:   Tue Jan 24 18:58:56 2017 -0500
    
        List comprehensions to process shadow values

---
+++ ledger_shadow.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+
+class Posting:
+    def __init__(self, account, value):
+        self.account = account
+        self.value = value
+
+    def __str__(self):
+        return '    {}  ${:.2f}'.format(self.account, self.value)
+
+class RealPosting(Posting):
+    def __init__(self, account, value, shadow_spec):
+        super().__init__(account, value)
+        self.shadow_spec = shadow_spec
+
+    def __str__(self):
+        s = [ super().__str__() ]
+        if self.shadow_spec is not None and len(self.shadow_spec) > 0:
+            s.append("    ; {}".format(dict(self.shadow_spec)))
+            shadow_postings = self.generate_shadow_postings()
+            if shadow_postings is not None and len(shadow_postings) > 0:
+                for p in shadow_postings:
+                    s.append(str(p))
+        return '\n'.join(s)
+
+    def print_shadow_postings(self):
+        shadow_postings = self.generate_shadow_postings()
+        if shadow_postings is not None:
+            for p in shadow_postings:
+                print(p)
+
+    def generate_shadow_postings(self):
+        if self.shadow_spec is None:
+            return
+        if not self.has_complete_shadow_spec():
+            self.resolve_elided_shadow_value()
+        shadow_postings = []
+        keys = list(self.shadow_spec.keys())
+        if len(keys) == 0:
+            return
+        elif len(keys) == 1:
+            k_last = keys[0]
+        else:
+            # Calculate values for all but last owner
+            for k in keys[:-1]:
+                account = "[{}:{}]".format(k, self.account)
+                value = 1e-2 * self.shadow_spec[k] * self.value
+                p = Posting(account, value)
+                shadow_postings.append(p)
+            k_last = keys[-1]
+        # Set last owner's value to residual
+        account = "[{}:{}]".format(k_last, self.account)
+        value = self.value - sum([ p.value for p in shadow_postings ])
+        p = Posting(account, value)
+        shadow_postings.append(p)
+        return shadow_postings
+
+    def has_complete_shadow_spec(self):
+        return not bool([ v for v in self.shadow_spec.values() if v is None ])
+
+    def resolve_elided_shadow_value(self):
+        valueless_owners = [ k for k, v in self.shadow_spec.items()
+                             if v is None ]
+        if len(valueless_owners) == 0:
+            pass
+        elif len(valueless_owners) == 1:
+            k = valueless_owners[0]
+            explicit_total = sum([ v for k, v in self.shadow_spec.items()
+                               if v is not None ])
+            elided_value = 100 - explicit_total
+            self.shadow_spec[k] = elided_value
+        else:
+            raise Exception(
+                'Multiple shadow members do not have values: {}'.format(
+                    valueless_owners))
+
+class Xact:
+    def __init__(self):
+        # List of `Posting`s
+        self.real_postings = []
+        # OrderedDict of shadow owners and values
+        self.shadow_spec = OrderedDict()
+        self.shadow_postings = [] # List of account-value pairs
+
+    def clear(self):
+        del self.real_postings[:]
+        self.shadow_spec.clear()
+
+    def add_posting(self, account, value):
+        if type(value) is str:
+            value = Xact.interpret_value(value)
+        p = RealPosting(account, value, self.shadow_spec)
+        self.real_postings.append(p)
+
+    def add_shadow(self, shadow_spec):
+        self.shadow_spec.update(shadow_spec)
+
+    def resolve_elided_posting_value(self):
+        i_valueless = [ i for i, v in
+                        zip(range(len(self.real_postings)), self.real_postings)
+                        if v.value is None ]
+        if len(i_valueless) == 0:
+            pass
+        elif len(i_valueless) == 1:
+            v = -sum([ Xact.interpret_value(p.value) for p in self.real_postings
+                       if p.value is not None ])
+            i = i_valueless[0]
+            self.real_postings[i].value = v
+        else:
+            raise Exception(
+                'Multiple postings do not have values: {}, {}'.format(
+                    i_valueless, self.real_postings))
+
+    @staticmethod
+    def interpret_value(value):
+        if type(value) is str:
+            v = float(value.translate({ ord(c): None for c in '$,' }))
+        else:
+            v = value
+        return v
+
+    def print_shadow_postings(self):
+        self.resolve_elided_posting_value()
+        for p in self.real_postings:
+            p.print_shadow_postings()
+
+if __name__ == "__main__":
+    import argparse
+    import ast
+    import re
+
+    # Transaction begin pattern
+    trbegpat = re.compile(r'^\d')
+    # Transaction end pattern
+    trendpat = re.compile(r'(^[^\s]|^[\s]*$)')
+    # Real posting pattern
+    rpostpat = re.compile(r'^\s+(?P<account>[\w\-,]+(\s?[\w\-,:]+)*)(\s{2,}(?P<value>-?\$?-?[\d]+(,\d+)*(\.\d+)?))?')
+    # Shadow key pattern
+    shkeypat = re.compile(r';\s*Shadow\s*:(.*)')
+
+    parser = argparse.ArgumentParser(
+        description = 'Generate shadow postings for Ledger journal.')
+    parser.add_argument('journal', help = 'Ledger journal file')
+    args = parser.parse_args()
+    filepath = args.journal
+ 
+    xact = Xact()
+    bInXact = False
+    with open(filepath, 'r') as f:
+        for line in f:
+            if bInXact:
+                if trendpat.match(line):
+                    # At end of transaction
+                    xact.print_shadow_postings()
+                    bInXact = False
+                else:
+                    m = rpostpat.match(line)
+                    if m:
+                        xact.add_posting(m.group('account'), m.group('value'))
+                    else:
+                        m = shkeypat.search(line)
+                        if m:
+                            shadow = ast.literal_eval(m.group(1).strip())
+                            xact.add_shadow(shadow)
+
+            if not bInXact and trbegpat.match(line):
+                # At beginning of transaction
+                bInXact = True
+                xact.clear()
+                m = shkeypat.search(line)
+                if m:
+                    shadow = ast.literal_eval(m.group(1).strip())
+                    xact.add_shadow(shadow)
+
+            print(line, end='')
+
+    if bInXact:
+        xact.print_shadow_postings()
+        bInXact = False
